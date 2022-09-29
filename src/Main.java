5ac27cbf7dcbc308e785a204c37426f41e37c252
Özcan ÇAVGA <81030695+OzcanCAVGA@users.noreply.github.com>
2022-09-29 20:01:17 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+/*
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve
+girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp ekrana basan programı yazıyoruz.
+ */
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner yazici = new Scanner(System.in);
+        System.out.println("Lutfen bir cift veya 4'un kati sayi giriniz: ");
+        int sayi, toplam = 0;
+        sayi = yazici.nextInt();
+        while (sayi%2==0) {
+            if (sayi % 2 == 0 && sayi % 4 == 0) {
+                toplam += sayi;
+                sayi = yazici.nextInt();
+            }
+
+        }
+        System.out.println(toplam);
+
     }
 }
